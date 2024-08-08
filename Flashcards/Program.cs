ad93515fb73c7115682b2306723018c83aecad9c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-08-08 01:10:23 -0600

    feat(core): extract service configuration to a dedicated class
    This commit introduces a new `ServicesConfigurator` class, responsible for
    setting up all the necessary services for the application. This promotes
    better organization and separation of concerns, making the code more
    readable and maintainable.

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,14 +1,6 @@
-﻿using Flashcards.Database;
-using Flashcards.Enums;
-using Flashcards.Handlers;
-using Flashcards.Interfaces.Database;
+﻿using Flashcards.Enums;
 using Flashcards.Interfaces.Handlers;
-using Flashcards.Interfaces.Repositories;
-using Flashcards.Interfaces.View.Factories;
-using Flashcards.Repositories;
-using Flashcards.View;
-using Flashcards.View.Factories;
-using Flashcards.View.Factories.EntriesInitializers;
+using Flashcards.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Flashcards;
@@ -17,35 +9,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        var serviceCollection = new ServiceCollection();
-        ConfigureServices(serviceCollection);
+        var serviceCollection = ServicesConfigurator.ServiceCollection;
 
         var serviceProvider = serviceCollection.BuildServiceProvider();
         var mainMenuCommandFactory = serviceProvider.GetRequiredService<IMenuHandler<MainMenuEntries>>();
         mainMenuCommandFactory.HandleMenu();
-    }
-
-    private static void ConfigureServices(IServiceCollection services)
-    {
-        services.AddTransient<IConfigurationProvider, ConfigurationProvider>();
-        services.AddTransient<IConnectionProvider, ConnectionProvider>();
-        services.AddTransient<IDatabaseInitializer, DatabaseInitializer>();
-        services.AddTransient<IMenuEntries<MainMenuEntries>, MenuEntries<MainMenuEntries>>();
-        services.AddTransient<IMenuEntries<StackMenuEntries>, MenuEntries<StackMenuEntries>>();
-        services.AddTransient<IMenuEntries<FlashcardEntries>, MenuEntries<FlashcardEntries>>();
-        services.AddTransient<IMenuEntriesInitializer<MainMenuEntries>, MainMenuEntriesInitializer>();
-        services.AddTransient<IMenuEntriesInitializer<StackMenuEntries>, StacksMenuEntriesInitializer>();
-        services.AddTransient<IMenuEntriesInitializer<FlashcardEntries>, FlashcardsMenuEntriesInitializer>();
-        services.AddTransient<IMenuCommandFactory<MainMenuEntries>, MenuCommandFactory<MainMenuEntries>>();
-        services.AddTransient<IMenuCommandFactory<StackMenuEntries>, MenuCommandFactory<StackMenuEntries>>();
-        services.AddTransient<IMenuCommandFactory<FlashcardEntries>, MenuCommandFactory<FlashcardEntries>>();
-        services.AddTransient<IChoosalbeEntryHandler, ChoosableEntryHandler>();
-
-        services.AddSingleton<IDatabaseManager, DatabaseManager>();
-        services.AddSingleton<IFlashcardsRepository, FlashcardsRepository>();
-        services.AddSingleton<IStacksRepository, StacksRepository>();
-        services.AddSingleton<IMenuHandler<MainMenuEntries>, MenuHandler<MainMenuEntries>>();
-        services.AddSingleton<IMenuHandler<StackMenuEntries>, MenuHandler<StackMenuEntries>>();
-        services.AddSingleton<IMenuHandler<FlashcardEntries>, MenuHandler<FlashcardEntries>>();
     }
 }
